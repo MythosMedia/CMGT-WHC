07f12fde8eacc8d1e76fb43fe157174802164381
Aymsep <71574687+Aymsep@users.noreply.github.com>
2024-11-01 03:46:01 +0000

    update home page images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const images = [
-    'assets/img/own/slider3.jpg',
-    'assets/img/own/service1.jpeg',  // Add paths to more images
-    'assets/img/own/slider-2.jpg'
+    'https://res.cloudinary.com/df2fe2yzk/image/upload/w_1500,h_800/v1730428890/20230629_145910_jckkdj.webp',
+    'https://res.cloudinary.com/df2fe2yzk/image/upload/w_1500,h_800/v1730428878/20230629_144422_zntkr9.webp',  // Add paths to more images
+    'https://res.cloudinary.com/df2fe2yzk/image/upload/w_1500,h_800/v1730428889/20230629_142704_e0r8kj.webp'
   ];
   
   let currentIndex = 0;
@@ -19,3 +19,17 @@ const images = [
   
   // Set initial background image
   changeBackgroundImage();
+
+  function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('open');
+  }
+  document.addEventListener('click', (e) => {
+    const sidebar = document.getElementById('sidebar');
+    const hamburger = document.querySelector('.hamburger');
+    
+    if (!sidebar.contains(e.target) && !hamburger.contains(e.target)) {
+      sidebar.classList.remove('open');
+    }
+  });
+    
